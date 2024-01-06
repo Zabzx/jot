5c1ae30a397eb9cf3dbec45ac684c62ea871ce59
Ziabeher Phillips <zabq02@gmail.com>
2024-01-06 18:00:39 -0400

    notes api

--- server/controllers/notes.js
+++ server/controllers/notes.js
@@ -30,11 +30,35 @@ async function createNote(req, res) {
         time: formattedTime,
     });
 
-    console.log(formattedDate)
-
     try {
         const dataToSave = await data.save();
         res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+async function updateNote(req, res) {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await noteSchema.findByIdAndUpdate(
+            id, updatedData, options
+        );
+
+        res.send(result);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+async function deleteNote(req, res) {
+    try {
+        const id = req.params.id;
+        const data = await noteSchema.findByIdAndDelete(id)
+        res.send(`Note: ${data} deleted`);
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
@@ -44,4 +68,6 @@ module.exports = {
     getOneNote,
     createNote,
     getNotes,
+    updateNote,
+    deleteNote,
 }
--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { getOneNote, createNote, getNotes } = require("../controllers/notes");
+const { getOneNote, createNote, getNotes, updateNote, deleteNote } = require("../controllers/notes");
 const noteRouter = express.Router();
 
 noteRouter.get("/", getNotes);
 noteRouter.get("/:id", getOneNote);
 noteRouter.post("/", createNote);
-noteRouter.get("/", );
-noteRouter.get("/", );
+noteRouter.patch("/:id", updateNote);
+noteRouter.delete("/:id", deleteNote);
 
 module.exports = noteRouter;
